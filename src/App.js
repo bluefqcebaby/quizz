d8b7be5ce2d122728b05e224e2f991e73fcd43b9
bluefqcebaby <idi.v.srakengrad@gmail.com>
2022-02-22 19:16:45 +0300

    letsgo

--- src/App.js
+++ src/App.js
@@ -1,35 +1,85 @@
-import { useState, useEffect, useRef } from "react";
-import Questions from "./Questions";
+import { useState, useEffect } from "react";
+import Question from "./Question";
+import blob1 from "./img/blob1.png";
+import blob2 from "./img/blob2.png";
+
 const App = () => {
   const [isStart, setIsStart] = useState(false);
-  const [apiQuestions, setApiQuestions] = useState({});
+  const [apiData, setApiData] = useState([]);
+  const [questions, setQuestions] = useState([]);
+  const [formData, setFormData] = useState({
+    question0: "",
+    question1: "",
+    question2: "",
+    question3: "",
+    question4: "",
+  });
   const clickHandler = () => {
     setIsStart(true);
   };
+  const shuffle = (arr) => {
+    return arr.sort(() => Math.random() - 0.5);
+  };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
+  const checkChecked = (name, str) => formData[name] === str;
   useEffect(() => {
     const getApi = async () => {
       const response = await fetch(
         "https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple"
       );
       const result = await response.json();
-      setApiQuestions(result.results);
+      setApiData(result.results);
     };
     getApi();
   }, []);
-  console.log(apiQuestions);
+  useEffect(() => {
+    setQuestions(
+      apiData.map((item) =>
+        shuffle([item.correct_answer, ...item.incorrect_answers])
+      )
+    );
+  }, [apiData]);
+  console.log(apiData);
   return (
-    <main className="main">
-      {isStart ? (
-        <Questions data={apiQuestions} />
-      ) : (
-        <div className="box">
-          <h1 className="box__title">Quizz</h1>
-          <button className="box__btn" onClick={clickHandler}>
-            Start
-          </button>
-        </div>
-      )}
-    </main>
+    <>
+      <img className="blob1" src={blob1} alt="blob1" />
+      <img className="blob2" src={blob2} alt="blob2" />
+      <main className="main">
+        {isStart ? (
+          <form className="questions">
+            {apiData.map((item, index) => {
+              return (
+                <Question
+                  questions={questions[index]}
+                  correctAnswer={item.correct_answer}
+                  key={index}
+                  title={item.question}
+                  id={index}
+                  handleChange={handleChange}
+                  checkChecked={checkChecked}
+                />
+              );
+            })}
+            <button className="questions__btn">Check answers</button>
+          </form>
+        ) : (
+          <div className="box">
+            <h1 className="box__title">Quizz</h1>
+            <button className="box__btn" onClick={clickHandler}>
+              Start
+            </button>
+          </div>
+        )}
+      </main>
+    </>
   );
 };
 
--- src/Question.js
+++ src/Question.js
@@ -1,12 +1,33 @@
-const Question = () => {
+import parse from "html-react-parser";
+
+const Question = ({ questions, title, id, handleChange, checkChecked }) => {
+  const style = {
+    borderBottom: id === 4 ? "none" : "1px solid #d6dbf5",
+  };
   return (
-    <div className="question__box">
-      <h2 className="question__title">Some title</h2>
-      <div>
-        <label>
-          <input type="radio" name="question1"></input>
-        </label>
-      </div>
+    <div className="question">
+      <h2 className="question__title">{parse(title)}</h2>
+      <ul className="question__answers" style={style}>
+        {questions.map((item, index) => {
+          const ques = parse(item);
+          const name = `question${id}`;
+          return (
+            <li className="question__answer" key={`list${index}`}>
+              <label>
+                <input
+                  className="question__input"
+                  type="radio"
+                  name={name}
+                  value={ques}
+                  onChange={handleChange}
+                  checked={checkChecked(name, ques)}
+                />
+                <p className="question__text">{ques}</p>
+              </label>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
